86a7a96cee6c44d6223cdc6b1b3659c962ced2fe
hillakatki <hillakatki@gmail.com>
2016-02-13 16:12:58 -0500

    Functional Read More / Read Less buttons

--- js/index.js
+++ js/index.js
@@ -3,3 +3,30 @@ $('.sign-up-button').on('click', function() {
 	$('.sign-up-button').html("No Sign Ups!")
 });
 
+$('.more').hide();
+
+$('.read-less').hide();
+
+$('#1st-read-more').on('click', function() {
+	$('#1st-more').slideToggle();
+	$('#1st-read-more').hide();
+	$('#1st-read-less').show();
+});
+
+$('#1st-read-less').on('click', function() {
+	$('#1st-more').slideToggle();
+	$('#1st-read-less').hide();
+	$('#1st-read-more').show();
+});
+
+$('#2nd-read-more').on('click', function() {
+	$('#2nd-more').slideToggle();
+	$('#2nd-read-more').hide();
+	$('#2nd-read-less').show();
+});
+
+$('#2nd-read-less').on('click', function() {
+	$('#2nd-more').slideToggle();
+	$('#2nd-read-less').hide();
+	$('#2nd-read-more').show();
+});
